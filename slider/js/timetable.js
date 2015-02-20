89dbafef845a1d4df471ac27f9f0383e29c61e77
artemphys <artemphys@gmail.com>
2015-02-20 17:58:52 +0300

    таблица из SON

---
+++ slider/js/timetable.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    //формируем таблицу из JSON//
+    $('.list-inline li').click(function(){
+        if (($('table').length > 0)) {
+        $('table').remove();}
+        else {
+        $('.container').append('<table class="container-fluid table table-hover"></table>');}
+    var tableInfo = {};
+        tableInfo.getJson = function() {
+        $.getJSON('02172015.json', function(data){
+            data.forEach(tableInfo.addObject(data));
+        });
+    };
+        tableInfo.getJson();
+        tableInfo.addObject = function(data) {
+            for(var i=0; i<=3; ++i) { //добавляем строки в таблицу
+                $('table').append('<tr id="'+i+'"></tr>');
+                for (j=0; j<=3; j++) {
+                $('.table #'+i).append('<td>' + data.table1[i][j] + '</td>');};
+                $('.table #'+i).append('<td>' + '<a href="#" class="btn btn-sm btn-danger" type="button">toDo</a>' + '</td>');
+            };
+        };
+});
+
+        $('.container').on('click', '.btn-danger', function(){
+            $(this).removeClass("btn-danger");
+            $(this).addClass("btn-success");
+            $(this).text('Done');
+        });
+});
--- timetable/js/timetable.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.btn-danger').click(function(){
-        $(this).removeClass("btn-danger");
-        $(this).addClass("btn-success");
-        $(this).text('Done');
-    });
-});
