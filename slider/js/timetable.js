0dfc1c0aaf4aa7a39d84addf2ef2572315805ea5
artemphys <artemphys@gmail.com>
2015-02-24 15:18:55 +0300

    Таблицы верно формируются из JSON

--- slider/js/timetable.js
+++ slider/js/timetable.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
-    //формируем таблицу из JSON//
-    $('.list-inline li').click(function(){
-        var order = $('ul').sortable("toArray");
-        console.log(order[0]);
-        if (($('table').length > 0)) {
-        $('table').remove();}
-        else {
-        $('.container').append('<table class="container-fluid table table-hover"></table>');}
-    var tableInfo = {};
-        tableInfo.getJson = function() {
-        $.getJSON('02172015.json', function(data){
-            data.forEach(tableInfo.addObject(data));
-        });
-    };
-        tableInfo.getJson();
-        tableInfo.addObject = function(data) {
-            for(var i=0; i<=3; ++i) { //добавляем строки в таблицу
-                $('table').append('<tr id="'+i+'"></tr>');
-                for (j=0; j<=3; j++) {
-                $('.table #'+i).append('<td>' + data.table1[i][j] + '</td>');};
-                $('.table #'+i).append('<td>' + '<a href="#" class="btn btn-sm btn-danger" type="button">toDo</a>' + '</td>');
+    var sliderElems = $('.slider ul li');//Создаем массив из элементов слайдера
+    var sliderArr = $.makeArray(sliderElems);
+
+        //формируем таблицу из JSON//
+        $(sliderArr).click(function () {
+            var thisSlide = $((sliderArr[sliderElems], this)).index('li');//Номер элемента, на котором был клик
+            //console.log(thisSlide);
+            if (($('table').length > 0)) {
+                $('table').remove();
+            }
+            else {
+                $('.container').append('<table class="container-fluid table table-hover"></table>');
+            }
+
+            var tableInfo = {};
+            tableInfo.getJson = function () {
+                $.getJSON('02172015.json', function (data) {
+                    data.forEach(tableInfo.addObject(data));
+                });
             };
-        };
-});
+            tableInfo.getJson();
+
+            tableInfo.addObject = function (data){
+                for (var i = 0; i <= 3; ++i) { //добавляем строки в таблицу
+                    $('table').append('<tr id="' + i + '"></tr>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].number + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].time + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].name + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].money + '</td>');
+                        $('.table #' + i).append('<td>' + '<a href="#" class="btn btn-sm btn-danger" type="button">toDo</a>' + '</td>');
+                    };
+            };
+        });
 //действие по нажатию кнопки в таблице
         $('.container').on('click', '.btn-danger', function(){
             $(this).removeClass("btn-danger");
