956421dbbe8b145e16f05f0ff88677fcd3db9f9d
artemphys <artemphys@gmail.com>
2015-02-26 13:06:05 +0300

    wrap the functions in a plugins

--- slider/js/timetable.js
+++ slider/js/timetable.js
@@ -1,55 +1,73 @@
 $(document).ready(function() {
-    var sliderElems = $('.slider ul li');//Создаем массив из элементов слайдера
+    var sliderElems = $('.slider ul li');//Create an array of the elements of the slider
     var sliderArr = $.makeArray(sliderElems);
 
-    //формируем таблицу из JSON//
-    $(sliderArr).click(function () {
+    //Plugin to open the table
+    (function( $ ){
+        $.fn.openTable = function() {
 
-        $(sliderArr).css({"border-color": "transparent"});
-        $((sliderArr[sliderElems], this)).css({"border-color": "#5cb85c"});//выделяем цветом выбранный элемент
 
-        var thisSlide = $((sliderArr[sliderElems], this)).index('li');//Номер элемента, на котором был клик
-        //console.log(thisSlide);
-        $('table').remove();
-        $('.container').append('<table class="table table-hover"></table>');
+            //make a table using JSON//
+            $(sliderArr).click(function () {
 
-        var tableInfo = {};
-        tableInfo.getJson = function () {
-            $.getJSON('02172015.json', function (data) {
-                data.forEach(tableInfo.addObject(data));
+                $(sliderArr).css({"border-color": "transparent"});
+                $((sliderArr[sliderElems], this)).css({"border-color": "#5cb85c"});//border of the active element
+
+                var thisSlide = $((sliderArr[sliderElems], this)).index('li');//the number of the active element
+                //console.log(thisSlide);
+                $('table').remove();
+                $('.container').append('<table class="table table-hover"></table>');
+
+                var tableInfo = {};
+                tableInfo.getJson = function () {
+                    $.getJSON('02172015.json', function (data) {
+                        data.forEach(tableInfo.addObject(data));
+                    });
+                };
+                tableInfo.getJson();
+
+                //add the rows into the table
+                tableInfo.addObject = function (data) {
+                    for (var i = 0; i <= 3; ++i) {
+                        $('table').append('<tr id="' + i + '"></tr>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].number + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].time + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].name + '</td>');
+                        $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].money + '</td>');
+                        $('.table #' + i).append('<td>' + '<a href="#" class="btn btn-sm btn-danger" type="button">toDo</a>' + '</td>');
+                    }
+                    ;
+                };
             });
+            $(sliderArr[0]).click();//fix default state
         };
-        tableInfo.getJson();
+        return this;
+    })(jQuery);
+    $(sliderArr).openTable();
 
-        tableInfo.addObject = function (data) {
-            for (var i = 0; i <= 3; ++i) { //добавляем строки в таблицу
-                $('table').append('<tr id="' + i + '"></tr>');
-                $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].number + '</td>');
-                $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].time + '</td>');
-                $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].name + '</td>');
-                $('.table #' + i).append('<td>' + data.arr[thisSlide].table[i].money + '</td>');
-                $('.table #' + i).append('<td>' + '<a href="#" class="btn btn-sm btn-danger" type="button">toDo</a>' + '</td>');
-            }
-            ;
-        };
-    });
-//действие по нажатию кнопки в таблице
+//click on button in the table
     $('.container').on('click', '.btn-danger', function () {
         $(this).removeClass("btn-danger");
         $(this).addClass("btn-success");
         $(this).text('Done');
     });
-    $(sliderArr[0]).click();//fix default state
 
-    //date
-    for (var i = 0; i <= 11; ++i) {
-    var thisDay = $('.slider ul li .date').eq(i);
-    var thisWeek = $('.slider ul li .title').eq(i);
 
-    var day=moment().add('days', i).format("MMM Do YY");
-    var week=moment().add('days', i).format("ddd");
-        
-    $(thisDay).text(day, this);
-    $(thisWeek).text(week, this);
-}
+    //Plagin to set the date
+    (function( $ ) {
+        $.fn.setRealDate = function() {
+            for (var i = 0; i <= 11; ++i) {
+                var thisDay = $('.slider ul li .date').eq(i);
+                var thisWeek = $('.slider ul li .title').eq(i);
+
+                var day = moment().add('days', i).format("MMM Do YY");
+                var week = moment().add('days', i).format("ddd");
+
+                $(thisDay).text(day, this);
+                $(thisWeek).text(week, this);
+            }
+            return this;
+        };
+    })(jQuery);
+    $(sliderArr).setRealDate();
 });
