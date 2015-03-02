a7577aa6f9f3e9292e2e03563fd2c280e59fad6b
artemphys <artemphys@gmail.com>
2015-03-02 17:04:59 +0300

    10

--- slider/index.js
+++ slider/index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/timetable.html', function(request, response) {
+app.get('/public', function(request, response) {
   response.send("timetable.html");
 });
 
