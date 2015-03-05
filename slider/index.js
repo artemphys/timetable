04e4c39f1cc2147c1f5ea337b7589739c766fd79
artemphys <artemphys@gmail.com>
2015-03-05 13:41:33 +0300

    #16 herokuapp

--- slider/index.js
+++ slider/index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(function(){app.load('timetable.html');});
+  response.send('timetable.html');
 });
 
 app.listen(app.get('port'), function() {
