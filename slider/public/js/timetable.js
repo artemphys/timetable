822172d3fcde1e46a4377915bef2c157cca9d356
artemphys <artemphys@gmail.com>
2015-03-05 17:18:05 +0300

    #18 in Progress

--- slider/public/js/timetable.js
+++ slider/public/js/timetable.js
@@ -1,6 +1,54 @@
 $(document).ready(function() {
+
     var sliderElems = $('.slider ul li');//Create an array of the elements of the slider
     var sliderArr = $.makeArray(sliderElems);
+
+//dependence of the slider width on the number of items
+    $(function() {
+        resizeSlider();
+        $(window).resize(resizeSlider);
+    });
+    function resizeSlider() {
+        var shiftWidth = $('.shift').width(),
+            btnsWidth = 2*($('.btn').outerWidth(true)),
+            itemWidth = $('.item').outerWidth(true),
+            coefficient=((shiftWidth-btnsWidth)/itemWidth);
+        //console.log(coefficient);
+
+        coefficient = Math.floor(coefficient);
+        $('.slider').css({"width": coefficient * itemWidth + "px"});
+    }
+
+    var count = 1,
+        position = 0,
+        itemWidth = $('.item').outerWidth(true);//for slide;
+
+    $('.js-slide-left').click(function() {
+        //console.log(position);
+        if(position>=0) return false;
+
+        position = Math.min((position + (itemWidth)*count), 0);
+        $('.slider ul').css({"margin-left":position + 'px'});
+        return false;
+    });
+
+    $('.js-slide-right').click(function() {
+        //add the items into the slider
+        if(sliderArr.length<=30){
+            $('.slider ul').append('<li class="item newItem"><span class="date">NoDate</span><span class="title">NoDate</span></li>');
+            sliderArr[sliderArr.length] = $('.item .newItem');
+            $(sliderArr).setRealDate();
+        }
+        //stop scroll
+        if (position<=-(sliderArr.length-7)*itemWidth) {
+            return false;
+        };
+        position = position-itemWidth;
+        $('.slider ul').css({"margin-left": position + 'px'});
+        return false;
+    });
+
+    //////////////////////////////
 
     //Plugin to open the table
     (function( $ ){
